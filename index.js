a84c8d08090c44b5ff56feeba48f50bccf55adbc
HYPER12755 <hyper14917374@gmail.com>
2026-03-16 11:44:03 +0000

    refactor: move root-level utility files to appropriate directories (docs/status, scripts, dist)

--- index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-process.env.INFECTED_RUNTIME_MODE = 'production';
-
-import('./dist/index.js').catch((error) => {
-  console.error('Failed to start infected:', error);
-  process.exit(1);
-});
